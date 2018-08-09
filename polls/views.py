1792201d581cecf6e59aadcc56e861f9e0571a74
Nick12321 <Nick.thompson@rogers.com>
2018-08-08 22:12:06 -0400

    modified views, templates, and inputs

--- polls/views.py
+++ polls/views.py
@@ -1,16 +1,25 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
-from django.template import loader
+# from django.template import loader
 from .models import Question
+from django.http import Http404
 
 
-def index(request):
-    return HttpResponse("Hello world! You're at the polls index.")
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s. " % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+    
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    #     # question = Question.objects.get(pk=2)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+    # return render(request, 'polls/detail.html', {'question': question})
+    # return HttpResponse("You're looking at question %s. " % question_id)
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
@@ -21,12 +30,14 @@ def vote(request, question_id):
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {'latest_question_list': latest_question_list,}
-    return HttpResponse(template.render(context, request))
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+    # template = loader.get_template('polls/index.html')
+    # return HttpResponse(template.render(context, request))
     # output = ', '.join([q.question_text for q in latest_question_list])
     # return HttpResponse(output)
 
-
+# def index(request):
+#     return HttpResponse("Hello world! You're at the polls index.")
 
 # Create your views here.
